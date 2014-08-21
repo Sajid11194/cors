0e6126142aa32a81d4c3d22608186030e2f75aab
Jim Heising <jheising@redfoundry.com>
2014-08-21 15:38:01 -0700

    added AWS health checks

--- server.js
+++ server.js
@@ -1,49 +1,143 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var http = require('http');
+var config = require("./config");
+var url = require("url");
+var request = require("request");
+var throttle = require("tokenthrottle")({rate: config.max_requests_per_second});
 
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-function parseEnvList(env) {
-  if (!env) {
-    return [];
-  }
-  return env.split(',');
+function addCORSHeaders(res) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Headers", "Origin,Content-Type,Accept");
+	res.setHeader("Content-Type", "text/plain");
 }
 
-// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+function writeResponse(res, httpCode, body) {
+	res.statusCode = httpCode;
+	res.end(body);
+}
 
-var cors_proxy = require('./lib/cors-anywhere');
-cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+function sendInvalidURLResponse(res) {
+	return writeResponse(res, 404, "url must be in the form of /fetch/{some_url_here}");
+}
+
+function sendTooBigResponse(res) {
+	return writeResponse(res, 413, "the content in the request or response cannot exceed " + config.max_request_length + " characters.");
+}
+
+function getClientAddress(req) {
+	return (req.headers['x-forwarded-for'] || '').split(',')[0]
+		|| req.connection.remoteAddress;
+}
+
+function processRequest(req, res)
+{
+	addCORSHeaders(res);
+
+	// Return options pre-flight requests right away
+	if (req.method.toUpperCase() === "OPTIONS") {
+		return writeResponse(res, 204);
+	}
+
+	var result = config.fetch_regex.exec(req.url);
+
+	if (result && result.length == 2 && result[1]) {
+		var remoteURL;
+
+		try {
+			remoteURL = url.parse(decodeURI(result[1]));
+		}
+		catch (e) {
+			return sendInvalidURLResponse(res);
+		}
+
+		// We only support http and https
+		if (remoteURL.protocol != "http:" && remoteURL.protocol !== "https:") {
+			return writeResponse(res, 400, "only http and https are supported");
+		}
+
+		var proxyRequest = request({
+			url: remoteURL,
+			headers: req.headers,
+			method: req.method,
+			timeout: config.proxy_request_timeout_ms,
+			strictSSL : false
+		});
+
+		proxyRequest.on('error', function(err){
+
+			console.log(err);
+
+			if(err.code === "ENOTFOUND")
+			{
+				return writeResponse(res, 502, "host cannot be found.")
+			}
+			else
+			{
+				return writeResponse(res, 500);
+			}
+
+		});
+
+		var requestSize = 0;
+		var proxyResponseSize = 0;
+
+		req.pipe(proxyRequest).on('data', function(data){
+
+			requestSize += data.length;
+
+			if(requestSize >= config.max_request_length)
+			{
+				proxyRequest.end();
+				return sendTooBigResponse(res);
+			}
+		});
+
+		proxyRequest.pipe(res).on('data', function (data) {
+
+			proxyResponseSize += data.length;
+
+			if(proxyResponseSize >= config.max_request_length)
+			{
+				proxyRequest.end();
+				return sendTooBigResponse(res);
+			}
+		});
+	}
+	else {
+		return sendInvalidURLResponse(res);
+	}
+}
+
+http.createServer(function (req, res) {
+
+	// Process AWS health checks
+	if(req.url === "/health")
+	{
+		return writeResponse(res, 200);
+	}
+
+	var remoteIP = getClientAddress(req);
+
+	// Log our request
+	console.log("%s %s %s", (new Date()).toJSON(), remoteIP, req.method, req.url);
+
+	if(config.enable_rate_limiting)
+	{
+		throttle.rateLimit(remoteIP, function(err, limited) {
+			if (limited)
+			{
+				return writeResponse(res, 429, "enhance your calm");
+			}
+
+			processRequest(req, res);
+		})
+	}
+	else
+	{
+		processRequest(req, res);
+	}
+
+}).listen(config.port);
+
+console.log("thingproxy.freeboard.io started");
